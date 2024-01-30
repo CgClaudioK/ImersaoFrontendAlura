8c917e2d3048f7da874902b85977fc9f5e6a6e10
CgClaudioK <kosooskiclaudio@gmail.com>
2024-01-30 14:14:57 -0300

    App Imersão Frontend in React

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Sidebar from './Sidebar/Sidebar';
+import Main from './Main/Main';
+
+function App() {
+  return (
+    <>
+     <Sidebar />
+     <Header />
+     <Main />
+     <Footer />
+
+     </>
+  );
+}
+
+export default App;
